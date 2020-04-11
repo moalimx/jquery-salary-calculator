ab04be10ef03a81503b3c5f5d19d602cded598ab
Halima <stonemilah@gmail.com>
2020-04-11 17:02:05 -0500

    creating objects with employee info

---
+++ client.js
@@ -0,0 +1,33 @@
+console.log('in JS');
+
+$(document).ready(readyNow)
+
+function readyNow() {
+    console.log('im in JQ');
+    
+}
+
+// creating a object where I can pass the information given for the employees 
+
+let employeeInformation = [
+    {
+    firstName: 'Jen',
+    lastName: 'Barber',
+    idNumber:'4521',
+    jobTitle: 'Team Lead',
+    annualSalary: 80000
+},
+{
+    firstName: 'Maurice',
+    lastName: 'Moss',
+    idNumber:'8724',
+    jobTitle: 'Support Team',
+    annualSalary: 58000
+},
+{
+    firstName: 'Roy',
+    lastName: 'Smith',
+    idNumber:'9623',
+    jobTitle: 'Quality Assurance',
+    annualSalary: 48000
+}]//employeeInformation
