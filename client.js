d5b4cbee552cf1ee99bcc22a52a25f73e45d4dcf
Halima <stonemilah@gmail.com>
2020-04-11 20:04:20 -0500

    delete btn working

--- client.js
+++ client.js
@@ -2,36 +2,61 @@ console.log('in JS');
 $(document).ready(readyNow)
 
 function readyNow() {
-    $('#submitBtn').on('click',addToArray)
+    $('#submitBtn').on('click', addToArray)
+    $('#submitBtn').on('click', appendToDom)
+    $('tbody').on('click','#deleteBtn', deleteFromArray)
 }//end of readyNow Function
 
 let employeeArray = [];
 // // creating a object where I can pass the information given for the employees
-function addToArray(){
+function addToArray() {
     let objectEmployeeInformation = {
 
-    firstName: $('#firstName').val(),
-    lastName: $('#lastName').val(),
-    idNumber:$('#idNumber').val(),
-    jobTitle: $('#jobTitle').val(),
-    annualSalary: $('#annualSalary').val()
-}
-employeeArray.push(objectEmployeeInformation); 
-console.log('button is clicked');
-
+        firstName: $('#firstName').val(),
+        lastName: $('#lastName').val(),
+        idNumber: $('#idNumber').val(),
+        jobTitle: $('#jobTitle').val(),
+        annualSalary: $('#annualSalary').val()
+    }
+    employeeArray.push(objectEmployeeInformation);
+    // console.log('button is clicked');
 }// end addToArray 
 
-// end of Object Employee info
 
 
 
-//append information to DOM
-// function appendToDom() {
+
+//append information to DOM and empty inputs
+function appendToDom() {
+    let el = $('table tbody')
+    el.empty();
+    for (let i = 0; i < employeeArray.length; i++) {
+        el.append(
+            `<tr>
+            <td> ${employeeArray[i].firstName}</td>
+            <td>${employeeArray[i].lastName}</td>
+            <td>${employeeArray[i].idNumber}</td>
+            <td>${employeeArray[i].jobTitle}</td>
+            <td>${employeeArray[i].annualSalary}</td>
+            <td><button id= 'deleteBtn'>Delete</button></td>
+          </tr>`
+        )
+            $('#firstName').val(''),
+            $('#lastName').val(''),
+            $('#idNumber').val(''),
+            $('#jobTitle').val(''),
+            $('#annualSalary').val('')
+
+    }
+
+}
+
+function deleteFromArray() {
+    for (let i = 0; i < employeeArray.length; i++) {
+        $(this).closest('tr').remove()
+       //doesn't remove from array 
+    }
+    console.log('in Delete');
     
-// }
+}
 
-// //function to calculate monthly costs
-
-// function calculateMonthlyCosts() {
-    
-// }
