4dba8a438192f592dec7ae6b39c67318d3e417fc
Halima <stonemilah@gmail.com>
2020-04-11 19:05:33 -0500

    submit btn and addtoarray function working

--- client.js
+++ client.js
@@ -1,33 +1,37 @@
 console.log('in JS');
-
 $(document).ready(readyNow)
 
 function readyNow() {
-    $('#submitBtn').on('click',add)
-    
-    
+    $('#submitBtn').on('click',addToArray)
 }//end of readyNow Function
+
 let employeeArray = [];
-// creating a object where I can pass the information given for the employees 
-let employeeInformation = [
-    {
-    firstName: firstName,
-    lastName: lastName,
-    idNumber:idNumber,
-    jobTitle: jobTitle,
-    annualSalary: annualSalary
+// // creating a object where I can pass the information given for the employees
+function addToArray(){
+    let objectEmployeeInformation = {
+
+    firstName: $('#firstName').val(),
+    lastName: $('#lastName').val(),
+    idNumber:$('#idNumber').val(),
+    jobTitle: $('#jobTitle').val(),
+    annualSalary: $('#annualSalary').val()
 }
-]
+employeeArray.push(objectEmployeeInformation); 
+console.log('button is clicked');
+
+}// end addToArray 
+
+// end of Object Employee info
 
 
 
 //append information to DOM
-function appendToDom() {
+// function appendToDom() {
     
-}
+// }
 
-//function to calculate monthly costs
+// //function to calculate monthly costs
 
-function calculateMonthlyCosts() {
+// function calculateMonthlyCosts() {
     
-}
+// }
